306629beaccb945100fe3b36d9259a785dffd051
nithinK-142 <nithin.sagar.359@gmail.com>
2024-08-04 11:48:12 +0530

    fix: environments path.

--- src/components/branchlist/branchlist.component.ts
+++ src/components/branchlist/branchlist.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../environment/environment';
+import { environment } from '../../environments/environment';
 import { CommonModule } from '@angular/common';
 
 interface Branch {
