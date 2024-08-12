ad905149ae733ea2fb03b9e58811bb629422a219
nithinK-142 <nithin.sagar.359@gmail.com>
2024-08-12 15:08:19 +0530

    fix: build error.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BranchlistComponent } from "../components/branchlist/branchlist.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BranchlistComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
